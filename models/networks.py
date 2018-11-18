71982076da8429be1c9c5564eabef6d53c5b4f28
Nathan <nathan@Christophers-MacBook-Pro.local>
2018-11-17 17:02:17 -0800

    add functional

--- models/networks.py
+++ models/networks.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import functools
 from torch.autograd import Variable
 import numpy as np
+import torch.nn.functional as F
 
 ###############################################################################
 # Functions
